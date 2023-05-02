5cf573b5c064557720d53eaa74f6f9ff67d73ed9
edwardnicolash <edward.nicolash@muf.co.id>
2023-05-02 18:45:16 +0700

    Reason : resolve wrong copy for service impl

--- src/main/java/com/km/service/impl/DataAktivitasToSasaranService.java
+++
@@ -1,8 +0,0 @@
-package com.km.service.impl;
-
-import java.util.List;
-import java.util.Map;
-
-public interface DataAktivitasToSasaranService {
-	List<?> dataAktivitasToSasaran(Map<String, Object> parameter) throws Exception;
-}
