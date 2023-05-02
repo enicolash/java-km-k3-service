9f649521f7c32037bee335618b675a3be11606a8
edwardnicolash <edward.nicolash@muf.co.id>
2023-05-03 02:37:48 +0700

    Reason : change mapping dengan kondisi after meet 02 Mei 2023

--- src/main/java/com/km/constand/ConstandSql.java
+++ src/main/java/com/km/constand/ConstandSql.java
@@ -4,39 +4,42 @@ public class ConstandSql {
 	public static final String data_aktivitas_pekerjaan = "SELECT id,aktivitas_pekerjaan_code,aktivitas_pekerjaan_desc FROM alvia_km_k3.mst_aktivitas_pekerjaan  WHERE deleted = 'FALSE' ORDER BY id ASC";
 	
 	public static final String matriks_aktivitas_to_resiko = "SELECT MAUR.id,MAUR.aktivitas_pekerjaan_code,MAUR.bahaya_code,(SELECT B.bahaya_desc \r\n"
-			+ "			 FROM  alvia_km_k3.mst_bahaya B\r\n"
-			+ "			 WHERE B.bahaya_code = MAUR.bahaya_code) AS bahaya_desc,\r\n"
-			+ "			 (SELECT B.tipe_bahaya_desc \r\n"
-			+ "			 FROM  alvia_km_k3.mst_bahaya B\r\n"
-			+ "			 WHERE B.bahaya_code = MAUR.bahaya_code) AS tipe_bahaya_desc,\r\n"
-			+ "			 MAUR.resiko_code,(SELECT R.resiko_desc \r\n"
-			+ "			 FROM  alvia_km_k3.mst_resiko R\r\n"
-			+ "			 WHERE R.resiko_code = MAUR.resiko_code) AS resiko_desc,\r\n"
-			+ "			 MAUR.pengendalian_code,(SELECT PR.pengendalian_desc FROM alvia_km_k3.mst_pengendalian_resiko PR \r\n"
-			+ "			 WHERE PR.pengendalian_code = MAUR.pengendalian_code) AS pengendalian_desc,\r\n"
-			+ "			 MAUR.sasaran_code,(SELECT S.sasaran_desc FROM alvia_km_k3.mst_sasaran S\r\n"
-			+ "			 WHERE S.sasaran_code = MAUR.sasaran_code) AS sasaran_desc\r\n"
-			+ "			 FROM alvia_km_k3.mst_map_aktivitas_until_resiko MAUR  \r\n"
-			+ "			 WHERE MAUR.aktivitas_pekerjaan_code = ?";
+			+ "			 			 FROM  alvia_km_k3.mst_bahaya B\r\n"
+			+ "			 			 WHERE B.bahaya_code = MAUR.bahaya_code) AS bahaya_desc,\r\n"
+			+ "			 			 (SELECT B.tipe_bahaya_desc \r\n"
+			+ "			 			 FROM  alvia_km_k3.mst_bahaya B\r\n"
+			+ "			 			 WHERE B.bahaya_code = MAUR.bahaya_code) AS tipe_bahaya_desc,\r\n"
+			+ "			 			 MAUR.resiko_code,(SELECT R.resiko_desc \r\n"
+			+ "			 			 FROM  alvia_km_k3.mst_resiko R\r\n"
+			+ "			 			 WHERE R.resiko_code = MAUR.resiko_code) AS resiko_desc,\r\n"
+			+ "			 			 MAUR.pengendalian_code,(SELECT PR.pengendalian_desc FROM alvia_km_k3.mst_pengendalian_resiko PR \r\n"
+			+ "			 			 WHERE PR.pengendalian_code = MAUR.pengendalian_code) AS pengendalian_desc,\r\n"
+			+ "			 			 MAUR.sasaran_code,(SELECT S.sasaran_desc FROM alvia_km_k3.mst_sasaran S\r\n"
+			+ "			 			 WHERE S.sasaran_code = MAUR.sasaran_code) AS sasaran_desc\r\n"
+			+ "			 			 FROM alvia_km_k3.mst_map_aktivitas_until_resiko MAUR  \r\n"
+			+ "			 			 WHERE MAUR.aktivitas_pekerjaan_code = ?\r\n"
+			+ "						 AND MAUR.bahaya_code = ?";
 	
 	public static final String matriks_resiko_to_penyebab = "SELECT MARP.penyebab_code,(SELECT P.penyebab_desc \r\n"
 			+ "FROM  alvia_km_k3.mst_penyebab P\r\n"
 			+ "WHERE P.penyebab_code = MARP.penyebab_code) AS penyebab_desc\r\n"
 			+ "FROM alvia_km_k3.mst_map_resiko_to_penyebab MARP  \r\n"
 			+ "WHERE MARP.aktivitas_pekerjaan_code = ? \r\n"
-			+ "AND MARP.id_map_aktivitas_untill_resiko = ?";
+			+ "AND MARP.resiko_code = ?";
 	
 	public static final String matriks_map_program = "SELECT id,sasaran_code,sasaran_desc,sumber_daya_desc,jangka_waktu_desc,indikator_pencapaian_desc,monitoring_desc,penanggung_jawab_desc \r\n"
 			+ "FROM alvia_km_k3.mst_map_program \r\n"
 			+ "WHERE sasaran_code = ?";
 	
 	public static final String map_wbs_to_bahaya = "SELECT MWB.id,MWB.level_2_desc,MWB.level_3_desc,MWB.level_4_desc,MWB.alternatif_design_desc,MWB.aktivitas_pekerjaan_code,\r\n"
-			+ "(SELECT AP.aktivitas_pekerjaan_desc \r\n"
-			+ "FROM alvia_km_k3.mst_aktivitas_pekerjaan AP WHERE AP.aktivitas_pekerjaan_code = MWB.aktivitas_pekerjaan_code) AS aktivitas_pekerjaan_desc,AB.bahaya_code,\r\n"
-			+ "(SELECT B.bahaya_desc\r\n"
-			+ "FROM alvia_km_k3.mst_bahaya B WHERE B.bahaya_code = AB.bahaya_code)\r\n"
-			+ "FROM alvia_km_k3.mst_map_wbs_to_bahaya MWB\r\n"
-			+ "RIGHT JOIN alvia_km_k3.mst_aktivitas_bahaya AB\r\n"
-			+ "ON MWB.aktivitas_pekerjaan_code = AB.aktivitas_pekerjaan_code";
+			+ "			 (SELECT AP.aktivitas_pekerjaan_desc \r\n"
+			+ "			 FROM alvia_km_k3.mst_aktivitas_pekerjaan AP WHERE AP.aktivitas_pekerjaan_code = MWB.aktivitas_pekerjaan_code) AS aktivitas_pekerjaan_desc,AB.bahaya_code,\r\n"
+			+ "			 (SELECT B.bahaya_desc\r\n"
+			+ "			 FROM alvia_km_k3.mst_bahaya B WHERE B.bahaya_code = AB.bahaya_code),\r\n"
+			+ "			 AB.id as id_map_aktivitas_to_bahaya\r\n"
+			+ "			 FROM alvia_km_k3.mst_map_wbs_to_bahaya MWB\r\n"
+			+ "			 RIGHT JOIN alvia_km_k3.mst_aktivitas_bahaya AB\r\n"
+			+ "			 ON MWB.aktivitas_pekerjaan_code = AB.aktivitas_pekerjaan_code\r\n"
+			+ "			 ORDER BY AB.id";
 	
 }
--- src/main/java/com/km/dao/impl/DataAktivitasToSasaranDaoImpl.java
+++ src/main/java/com/km/dao/impl/DataAktivitasToSasaranDaoImpl.java
@@ -24,7 +24,10 @@ public class DataAktivitasToSasaranDaoImpl implements DataAktivitasToSasaranDao 
 		try {
 			connection.ps = connection.conn.prepareStatement(ConstandSql.matriks_aktivitas_to_resiko);
 			String aktivitas_code = parameter.get("aktivitas_pekerjaan_code") == null ? "" : parameter.get("aktivitas_pekerjaan_code").toString();
+			String bahaya_code = parameter.get("bahaya_code") == null ? "" : parameter.get("bahaya_code").toString();
+
 			connection.ps.setString(1, aktivitas_code);
+			connection.ps.setString(2, bahaya_code);
 			connection.rs = connection.ps.executeQuery();
 			response = convertToModelMatriksAktivitasToResiko(connection.rs,connection);
 		 } catch (Exception e) {
@@ -50,7 +53,7 @@ public class DataAktivitasToSasaranDaoImpl implements DataAktivitasToSasaranDao 
 	
 	public static ResponseDaoMatriksAktivitasToResiko queryDataToModelMatriks (ResultSet rs,Conn connection) throws Exception{
 		ResponseDaoMatriksAktivitasToResiko dataMatriks = new ResponseDaoMatriksAktivitasToResiko();
-		Integer idFromQuery = rs.getInt("id");
+//		Integer idFromQuery = rs.getInt("id");
 		String aktivitasPekerjaanCodeFromQuery = rs.getString("aktivitas_pekerjaan_code") == null ? "" :  rs.getString("aktivitas_pekerjaan_code") .toString();
 		String bahayaCodeFromQuery = rs.getString("bahaya_code") == null ? "" :  rs.getString("bahaya_code") .toString();
 		String bahayaDescFromQuery = rs.getString("bahaya_desc") == null ? "" :  rs.getString("bahaya_desc") .toString();
@@ -61,7 +64,7 @@ public class DataAktivitasToSasaranDaoImpl implements DataAktivitasToSasaranDao 
 		String pengendalianDescFromQuery = rs.getString("pengendalian_desc") == null ? "" :  rs.getString("pengendalian_desc") .toString();
 		String sasaranCodeFromQuery = rs.getString("sasaran_code") == null ? "" :  rs.getString("sasaran_code") .toString();
 		String sasaranDescFromQuery = rs.getString("sasaran_desc") == null ? "" :  rs.getString("sasaran_desc") .toString();
-		dataMatriks.setId_map_aktivitas_untill_resiko(idFromQuery);
+//		dataMatriks.setId_map_aktivitas_untill_resiko(idFromQuery);
 		dataMatriks.setAktivitas_pekerjaan_code(aktivitasPekerjaanCodeFromQuery);
 		dataMatriks.setBahaya_code(bahayaCodeFromQuery);
 		dataMatriks.setBahaya_desc(bahayaDescFromQuery);
@@ -78,15 +81,12 @@ public class DataAktivitasToSasaranDaoImpl implements DataAktivitasToSasaranDao 
 	
 	public static List<String> dataResikoToPenyebab(ResponseDaoMatriksAktivitasToResiko parameter,Conn connection)throws Exception{
 		List<String> response = new ArrayList<>();
-		String id_map_aktivitas_untill_resiko = parameter.getId_map_aktivitas_untill_resiko() == null ? "" : parameter.getId_map_aktivitas_untill_resiko().toString();
-		if(id_map_aktivitas_untill_resiko.equals("14")) {
-			System.out.println(id_map_aktivitas_untill_resiko);
-		}
+		String resiko_code = parameter.getResiko_code() == null ? "" : parameter.getResiko_code().toString();
 		String aktivitas_pekerjaan_code =  parameter.getAktivitas_pekerjaan_code() == null ? "" : parameter.getAktivitas_pekerjaan_code();
 		try {
 			connection.ps = connection.conn.prepareStatement(ConstandSql.matriks_resiko_to_penyebab);
 			connection.ps.setString(1, aktivitas_pekerjaan_code);
-			connection.ps.setString(2, id_map_aktivitas_untill_resiko);
+			connection.ps.setString(2, resiko_code);
 			connection.rs = connection.ps.executeQuery();
 			response = convertQueryDataToListPenyebab(connection.rs);
 		 } catch (Exception e) {
